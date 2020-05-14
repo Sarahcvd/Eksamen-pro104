2209d47e29b7e2b6c9bfe89f376f2dec80b38caa
Espen Bakken <bakken.espen@gmail.com>
2020-05-14 17:30:23 +0200

    Kommentert koden

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 $(window).load(function () {
+    //Ved klikk på denne classen, skal hover_bkgr_fricc vises. 
     $(".trigger_popup_fricc").click(function(){
        $('.hover_bkgr_fricc').show();
     });
+    // Ved klikk på krysset, skal hover_bkgr_fricc skjules. 
     $('.popupCloseButton').click(function(){
         $('.hover_bkgr_fricc').hide();
     });
 });
+
+function createNewElement() {
+    //Først lages en div
+	var txtNewInputBox = document.createElement('div');
+
+    // Så legges innhold til i denne diven
+	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox' class='field' placeholder='What is your new task?'>";
+
+    //Dermed legges txtNewInputBox til som ny child
+	document.getElementById("inputField").appendChild(txtNewInputBox);
+}
