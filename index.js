d0b860ca79a519c85b40309dd04ee9488d26ed68
Espen Bakken <bakken.espen@gmail.com>
2020-05-14 20:59:59 +0200

    Lagt inn funksjonalitet for tasks

--- index.js
+++ index.js
@@ -11,11 +11,55 @@ $(window).load(function () {
 
 function createNewElement() {
     //Først lages en div
-	var txtNewInputBox = document.createElement('div');
+    var x2 = document.getElementById("x2");
+    var x = document.getElementById("inputField");
+    x2.style.display = "none";
+    x.style.display = "block";
+    
+    
+    var txtNewInputBox = document.createElement('FORM');
+    txtNewInputBox.setAttribute("onsubmit", "createNewTask(event)")
+    txtNewInputBox.setAttribute("id", "x")
 
     // Så legges innhold til i denne diven
-	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox' class='field' placeholder='What is your new task?'>";
-
+    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox' class='field' placeholder='What is your new task?'><input type='submit' id='submit-btn' value='Add'>";
     //Dermed legges txtNewInputBox til som ny child
-	document.getElementById("inputField").appendChild(txtNewInputBox);
+    document.getElementById("inputField").appendChild(txtNewInputBox);
+}
+// Kode dersom enter trykkes
+$(document).ready(function(){
+    $('#newInputBox').keypress(function(ev){
+      //If user pressed Enter Key then trigger Submit click
+      if(ev.keyCode==13)
+      $('#submit-btn').click();
+    });
+});
+
+function createNewTask(event){
+    var x = document.getElementById("inputField");
+    const name = document.getElementById("newInputBox").value;
+    const product = {name};
+
+    const outputTask = JSON.parse(window.localStorage.getItem("outputTask")) || [];
+    outputTask.push(product);
+    window.localStorage.setItem("outputTask", JSON.stringify(outputTask)); 
+    renderTasks();
+
+    event.target.reset();
+    x.style.display = "none";
+    x2.style.display = "block";
+    
+}
+
+function renderTasks(){
+    const outputTask = JSON.parse(window.localStorage.getItem("outputTask")) || [];
+    const outputTaskEl = document.getElementById("outputTasks");
+    outputTaskEl.innerHTML = "";
+    for(const product of outputTask){
+        const productTwo = document.createElement("div");
+        const {name} = product;
+        
+        productTwo.innerHTML = "<div id='task'><h4>" + product.name + "</h4>" + "</div>" ;
+        outputTaskEl.appendChild(productTwo);
+    }
 }
